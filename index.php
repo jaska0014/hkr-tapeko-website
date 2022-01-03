fe7d0f6ca2fc251b02e965b5155878e5bb3dbe03
Jasmine Skarpfors <j.skarpfors@hotmail.com>
2022-01-03 00:19:24 -0500

    latest update to the nav

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+  <meta charset="utf-8">
+  <title>TAPEKO</title>
+
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/css/all.min.css">
+  <link rel="stylesheet" href="assets/css/custom.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
+</head>
+<body>
+    <nav class="navbar navbar-light ">
+    <header class="container pt-8 pb-0 mb-8"> 
+        <div class= nav navbar-left>
+              <a class="navbar-brand me-5" href="index.html">
+            <img src="assets/images/tapeko.png" height="40" alt="TAPEKO logo">
+          </a>
+                  </div>
+                  <button class="navbar-toggler d-flex flex-column justify-content-around" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="toggler-icon top-bar"></span>
+                    <span class="toggler-icon middle-bar"></span>
+                    <span class="toggler-icon bottom-bar"></span>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                      <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">Features</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">Pricing</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                      </li>
+                    </ul>
+                  </div>
+                  <!-- <div class="nav navbar-right ms-5">
+                  <button type="button" class="btn btn-link text-dark text-decoration-none"data-toggle="modal" data-target="#signupModal"type="submit">JOIN US</button>
+                  <button type="button" class="btn btn-link text-dark text-decoration-none" data-toggle="modal" data-target="#loginModal"type="submit ">LOGIN</button>
+           <a href="add.php" type="submit" class="btn btn-link text-dark text-decoration-none">CREATE </a>
+           <form class="d-flex">
+            <input class="form-control me-2" type="search" placeholder="Search"  aria-label="Search" >
+          </form> -->
+
+        </nav>		
+      </div>	
+      <nav class="smulor"aria-label="breadcrumb">
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item active ms-5" aria-current="page">Home</li>
+          <button class="nav navbar-right ms-5 btn btn-link text-white text-decoration-none" type="submit"><i class="fas fa-search mr-1"></i></button>
+        </ol>
+      </nav>
+
+      
+	</body>
+
+
