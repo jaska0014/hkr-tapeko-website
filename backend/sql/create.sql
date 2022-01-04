7850665905e6bda8289d797519b42343fc0f45c0
Keith Leo-Smith <keith@ideamconsult.com>
2022-01-04 08:40:42 +0200

    added sql

--- backend/sql/create.sql
+++ backend/sql/create.sql
@@ -3,19 +3,20 @@ USE `hkr-tapeko`;
 DROP TABLE IF EXISTS `products`;
 CREATE TABLE `products` 
 ( 
-    `product_id` BIGINT NOT NULL AUTO_INCREMENT , 
-    `product_name` VARCHAR(50) NOT NULL , 
-    `product_type` VARCHAR(50) NOT NULL , 
-    `product_desc` VARCHAR(1000) NOT NULL , 
-    `product_designer` VARCHAR(50) NOT NULL , 
-    `product_price` DECIMAL(18,2) NOT NULL , 
-    `product_year` INT NOT NULL , 
-    `product_location` VARCHAR(50) NOT NULL , 
-    PRIMARY KEY (`product_id`)
-  )
+  `product_id` BIGINT NOT NULL AUTO_INCREMENT , 
+  `product_name` VARCHAR(50) NOT NULL , 
+  `product_type` VARCHAR(50) NOT NULL , 
+  `product_desc` VARCHAR(1000) NOT NULL , 
+  `product_designer` VARCHAR(50) NOT NULL , 
+  `product_price` DECIMAL(18,2) NOT NULL , 
+  `product_year` INT NOT NULL , 
+  `product_location` VARCHAR(50) NOT NULL , 
+  PRIMARY KEY (`product_id`)
+);
 
 DROP TABLE IF EXISTS `product_images`; 
-CREATE TABLE `product_images` (
+CREATE TABLE `product_images` 
+(
   `product_id` BIGINT NOT NULL,
   `image_url` varchar(500) NOT NULL DEFAULT 'default.jpg'
-)
+);
--- backend/sql/insert.sql
+++ backend/sql/insert.sql
@@ -1,2 +1,23 @@
+USE `hkr-tapeko`;
 
--- Add Insert here
+INSERT INTO `products` 
+( 
+    `product_id`,
+    `product_name`,
+    `product_type`,
+    `product_desc`,
+    `product_designer`, 
+    `product_price`,
+    `product_year`,
+    `product_location`
+)
+values
+(1, '2st bruna Pernilla 1969', 'Fåtölj', 'Nyrenoverad snurrfåtölj med ny bärande väv och ny DUX original kuddsats i tobaksbrunt skinn Elmotique.', 'DUX', 27000, 1969, 'Stockholm'),
+(2, 'Pernilla med fotpall', 'Fåtölj', 'Ett renoveringsobjekt som fortfarande går att använda som den är. Kostnad för en helrenovering ca. 18.000:- ink. moms. Designer: Alf Svensson 1950-talet', 'Alf Svensson', 18000, 1960, 'Göteborg'),
+(3, 'Liggfåtölj Pernilla 303', 'Fåtölj', 'Nyrenoverad snurrfåtölj med ny bärande väv och ny DUX original kuddsats i tobaksbrunt skinn Elmotique.', 'Bruno Mathsson', 31000, 1969, 'Göteborg'),
+(4, 'Ritzy soffa', 'Soffa', 'Fusce dapibus, tellus ac cursus commodo, tortor mauris ', 'Bruno Mathsson', 42000, 1970, 'Stockholm'),
+(5, 'Fåtöljer, 2st Ingrid', 'Fåtölj', 'Ett renoveringsobjekt som fortfarande går att använda som den är. Kostnad för en helrenovering ca. 18.000:- ink. moms. Designer: Alf Svensson 1950-talet', 'Bruno Mathsson', 32000, 1969, 'Stockholm'),
+(6, 'Hans Wegner 4-sits soffa', 'Soffa', 'Samtliga dynor av hög kvalitet, resårdynor och nyrenoverade. Stoppningen är skyddad av en kraftig bomullstwill. Synliga trädetaljer är i teak.', 'Hans Wegner', 41000, 1948, 'Göteborg'),
+(7, 'Fällbord i vitt hårdlaminat', 'Bord', 'Bordsskivor i vitt hårdlaminat och underrede i björk. Levereras monterat, ihopfällt i ett säkert emballage.', 'Bruno Mathsson', 38150, 1935, 'Uppsala'),
+(8, 'Runt barbord NON', 'Bord', 'Stomme av stål ingjuten i svart hårdgummi. (Barbordet i mitten på fotot)', 'Källemo', 10400, 2000, 'Malmö')
+;
