fd7da5e8d238060568121aa92d0506c8b6fa47e2
Jasmine Skarpfors <j.skarpfors@hotmail.com>
2022-01-04 07:52:52 -0500

    footer

--- index.php
+++ index.php
@@ -10,14 +10,14 @@
   <link rel="stylesheet" href="assets/css/custom.css">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500&display=swap" rel="stylesheet">
 </head>
 
 <body>
 
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container fluid"> 
-            <a class="navbar-brand" href="index.php"><img src="assets/images/tapeko.png" height="40" alt="TAPEKO logo"></a>
+            <a class="navbar-brand" href="index.php"><img src="assets/images/tapeko.png" height="50" alt="TAPEKO logo"></a>
                 <button class="navbar-toggler collapsed d-flex d-lg-none flex-column justify-content-around" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="toggler-icon top-bar"></span>
                     <span class="toggler-icon middle-bar"></span>
@@ -56,6 +56,81 @@
   </div>
       
   <script src="assets/js/bootstrap.bundle.min.js"></script>
+
+
+
+<!-- Footer -->
+<footer class=" text-start" style="background-color:#ededed;">
+  <!-- Grid container -->
+  <div class="container mt-5 pt-5 pb-5">
+
+ <!-- Section: Links -->
+    <section class="">
+      <!--Grid row-->
+      <div class="row">
+               <!--Grid column-->
+               <div class="col-lg-3 col-md-10 mb-10 mb-md-10">
+          <h6 class="text-uppercase pt-5">BEGAGNADE MÖBLER</h6>
+
+          <ul class="list-unstyled mb-1">
+            <li>
+              <a href="#!" class="text-dark text-decoration-none">Köp begagnade designmöbler</a>
+            </li>
+          </ul>
+          <hr class="divider pb-5">
+        </div>
+        <!--Grid column-->
+         <!--Grid column-->
+         <div class="col-lg-3 col-md-10 mb-10 mb-md-10">
+          <h6 class="text-uppercase pt-5">VÅRA TJÄNSTER</h6>
+
+          <ul class="list-unstyled mb-1">
+            <li>
+              <a href="#!" class="text-dark text-decoration-none">Vad vi kan hjälpa till med</a>
+            </li>
+          </ul>
+          <hr class="divider pb-5">
+        </div>
+        <!--Grid column-->
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-10 mb-10 mb-md-10">
+          <h6 class="text-uppercase pt-5">MÖBELTAPETSERARE</h6>
+
+          <ul class="list-unstyled mb-1">
+            <li>
+              <a href="#!" class="text-dark text-decoration-none">Hitta en möbeltapetserare</a>
+            </li>
+          </ul>
+          <hr class="divider pb-5">
+        </div>
+        <!--Grid column-->
+         <!--Grid column-->
+         <div class="col-lg-3 col-md-10 mb-10 mb-md-10">
+          <h6 class="text-uppercase pt-5">SOCIALA MEDIER</h6>
+
+          <ul class="list-unstyled mb-1">
+            <li>
+              <a href="#!" class="text-dark text-decoration-none">Följ oss på sociala medier</a>
+            </li>
+          <hr class="divider">
+          <i class="fab fa-twitter fa-2x"></i>
+          <i class="fab fa-instagram fa-2x"></i>
+          <i class="fab fa-facebook fa-2x"></i>
+        
+          </ul>
+        </div>
+        <!--Grid column-->
+
+      </div>
+      <!--Grid row-->
+    </section>
+    <!-- Section: Links -->
+
+  </div>
+  <!-- Grid container -->
+
+</footer>
+<!-- Footer -->
 
 </body>
 
--- product-detail.php
+++ product-detail.php
@@ -10,7 +10,6 @@
   <link rel="stylesheet" href="assets/css/custom.css">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
 </head>
 
 <body>
