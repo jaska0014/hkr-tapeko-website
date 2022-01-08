78101505674f8d1fecdb20be950023939d865407
Jasmine Skarpfors <j.skarpfors@hotmail.com>
2022-01-08 13:19:43 -0500

    product info

--- backend/sql/insert-products.sql
+++ backend/sql/insert-products.sql
@@ -14,12 +14,12 @@ INSERT INTO `products`
     `image_url`
 )
 values
-(1, '2st bruna Pernilla 1969', 'Fåtölj', 'Nyrenoverad snurrfåtölj med ny bärande väv och ny DUX original kuddsats i tobaksbrunt skinn Elmotique.', 'DUX', 27000, 1969, 'Stockholm', 'poduct-images/default/default-product-1.jpg'),
-(2, 'Pernilla med fotpall', 'Fåtölj', 'Ett renoveringsobjekt som fortfarande går att använda som den är. Kostnad för en helrenovering ca. 18.000:- ink. moms. Designer: Alf Svensson 1950-talet', 'Alf Svensson', 18000, 1960, 'Göteborg', 'poduct-images/default/default-product-2.jpg'),
-(3, 'Liggfåtölj Pernilla 303', 'Fåtölj', 'Nyrenoverad snurrfåtölj med ny bärande väv och ny DUX original kuddsats i tobaksbrunt skinn Elmotique.', 'Bruno Mathsson', 31000, 1969, 'Göteborg', 'poduct-images/default/default-product-3.jpg'),
-(4, 'Ritzy soffa', 'Soffa', 'Fusce dapibus, tellus ac cursus commodo, tortor mauris ', 'Bruno Mathsson', 42000, 1970, 'Stockholm', 'poduct-images/default/default-product-4.jpg'),
-(5, 'Fåtöljer, 2st Ingrid', 'Fåtölj', 'Ett renoveringsobjekt som fortfarande går att använda som den är. Kostnad för en helrenovering ca. 18.000:- ink. moms. Designer: Alf Svensson 1950-talet', 'Bruno Mathsson', 32000, 1969, 'Stockholm', 'poduct-images/default/default-product-5.jpg'),
-(6, 'Hans Wegner 4-sits soffa', 'Soffa', 'Samtliga dynor av hög kvalitet, resårdynor och nyrenoverade. Stoppningen är skyddad av en kraftig bomullstwill. Synliga trädetaljer är i teak.', 'Hans Wegner', 41000, 1948, 'Göteborg', 'poduct-images/default/default-product-6.jpg'),
-(7, 'Fällbord i vitt hårdlaminat', 'Bord', 'Bordsskivor i vitt hårdlaminat och underrede i björk. Levereras monterat, ihopfällt i ett säkert emballage.', 'Bruno Mathsson', 38150, 1935, 'Uppsala' , 'poduct-images/default/default-product-7.jpg'),
-(8, 'Runt barbord NON', 'Bord', 'Stomme av stål ingjuten i svart hårdgummi. (Barbordet i mitten på fotot)', 'Källemo', 10400, 2000, 'Malmö', 'poduct-images/default/default-product-8.jpg')
+(1, 'Svart Jetson 1969', 'Fåtölj', 'Nyrenoverad snurrfåtölj med ny bärande väv och ny DUX original kuddsats i svart skinn Elmotique.', 'DUX', 27000, 1969, 'Stockholm', 'poduct-images/default/default-product-1.jpg'),
+(2, 'Klassisk Karin Fåtölj', 'Fåtölj', 'Ett renoveringsobjekt som fortfarande går att använda som den är. Original av Alf Svensson 1950-talet', 'Alf Svensson', 18000, 1950, 'Göteborg', 'poduct-images/default/default-product-2.jpg'),
+(3, 'Fåtölj Pernilla 303', 'Fåtölj', 'Nyrenoverad snurrfåtölj med ny bärande väv och ny DUX original kuddsats i grå väv.', 'Bruno Mathsson', 31000, 1969, 'Göteborg', 'poduct-images/default/default-product-3.jpg'),
+(4, 'Ritzy soffa', 'Soffa', 'Fusce dapibus, tellus ac cursus commodo, tortor mauris', 'Bruno Mathsson', 42000, 1970, 'Stockholm', 'poduct-images/default/default-product-4.jpg'),
+(5, 'Hans Wegner 3-sits soffa', 'Soffa', 'Samtliga dynor av hög kvalitet, resårdynor och nyrenoverade. Stoppningen är skyddad av en kraftig bomullstwill. Synliga trädetaljer är i teak.', 'Hans Wegner', 41000, 1948, 'Göteborg', 'poduct-images/default/default-product-5.jpg'),
+(6, 'Vackert träbord', 'Bord', 'Stomme av ek, detaljer i teak.', 'Källemo', 10400, 2000, 'Malmö', 'poduct-images/default/default-product-6.jpg')
+(7, 'Fåtölj med fotpall', 'Fåtölj', 'Ett renoveringsobjekt som fortfarande går att använda som den är.', 'Alf Svensson', 18000, 1960, 'Stockholm', 'poduct-images/default/default-product-7.jpg'),
+(8, '2-sits lädersoffa', 'Soffa', 'Fusce dapibus, tellus ac cursus commodo, tortor mauris imsum ipsum.', 'DUX', 38150, 1935, 'Uppsala' , 'poduct-images/default/default-product-8.jpg'),
 ;
