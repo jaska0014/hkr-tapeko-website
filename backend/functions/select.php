bb9e151b89c0650db56df978899db89b47099c7b
Keith Leo-Smith <keith@ideamconsult.com>
2022-01-08 21:12:52 +0200

    php

--- backend/functions/select.php
+++ backend/functions/select.php
@@ -1,6 +1,18 @@
 <?php
-    //Get all products and order by date added
-    $sql = 'SELECT * FROM products order by date_added DESC';
-    $stmt = $dbh->prepare($sql);
-    $stmt->execute();
+
+    // Checks whether where is set on the url
+    if (isset($_GET['where']))
+    {
+        $sql = 'SELECT * FROM products WHERE product_type = :where_filter_var order by date_added DESC';
+        $stmt = $dbh->prepare($sql);
+        $stmt->bindValue(':where_filter_var', $_GET['where']);
+        $stmt->execute();
+    }
+    else
+    {
+        $sql = 'SELECT * FROM products order by date_added DESC'; 
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute();
+    }
+
 ?>
